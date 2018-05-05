095d89406f112a81f9b24cca9816ddd8cdd126aa
CANER\PC <caner.sezginer@ug.bilkent.edu.tr>
2018-05-06 02:23:20 +0300

    Cake

--- hello.java
+++ hello.java
@@ -5,5 +5,7 @@ public class hello{
 	public static void main(String[] args){
 	System.out.println("hello");
 	System.out.println("Manyank kod yazarım!");
+	System.out.println("Manyakça kodladım");
+	
 	}
 }
